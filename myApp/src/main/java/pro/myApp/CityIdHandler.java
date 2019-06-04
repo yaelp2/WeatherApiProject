324c5b33ec1bee046d711ef26e707f2126d68757
pesso <pesso@pesso>
2019-06-04 15:26:45 +0300

    findCityById() has been changed
    Signed-off-by: pesso <pesso@pesso>

--- myApp/src/main/java/pro/myApp/CityIdHandler.java
+++ myApp/src/main/java/pro/myApp/CityIdHandler.java
@@ -1,5 +1,7 @@
 package pro.myApp;
 
+import java.util.HashMap;
+
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -7,16 +9,28 @@ import org.json.simple.parser.ParseException;
 import io.vertx.core.Vertx;
 
 public class CityIdHandler {
-	private org.json.simple.JSONArray jsonArr; 
-	Vertx vertx = Vertx.vertx();
+	private static HashMap<String,Long> map;
 	
 	
 	public CityIdHandler() {
-		JSONParser parser = new JSONParser();
+		map = new HashMap<String,Long>();
+		Vertx vertx = Vertx.vertx();
+		JSONParser parser = new JSONParser();	
 		vertx.fileSystem().readFile("src/main/resources/cities.json", result -> {
 			if(result.succeeded()) {
 				try {
+					org.json.simple.JSONArray jsonArr;
 					jsonArr = (JSONArray) parser.parse(result.result().toString());
+					org.json.simple.JSONObject json;
+					String key;
+					for(int i=0; i<jsonArr.size(); ++i) {
+							json = (org.json.simple.JSONObject) jsonArr.get(i);
+							key = json.get("country").toString().toLowerCase()+json.get("name").toString().toLowerCase();
+							map.put(key, (Long)json.get("id"));
+
+						}
+					
+					
 				} catch (ParseException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -29,19 +43,31 @@ public class CityIdHandler {
 		
 	}
 	
-	public long findCityId(String country, String city) {
-		long id = -1;
-		for(int i=0; i<jsonArr.size(); ++i) {
+//	public long findCityId(String country, String city) {
+//		long id = -1;
+//		for(int i=0; i<jsonArr.size(); ++i) {
+//	
+//			org.json.simple.JSONObject json = (org.json.simple.JSONObject) jsonArr.get(i);
+//			if(json.get("country").toString().toLowerCase().equals(country.toLowerCase()) && json.get("name").toString().toLowerCase().equals(city.toLowerCase())){
+//				id = (long)json.get("id");
+//				break;
+//			}
+//		
+//			
+//		}
+//		return id;
+//			
+//
+//
+//	}
 	
-			org.json.simple.JSONObject json = (org.json.simple.JSONObject) jsonArr.get(i);
-			if(json.get("country").toString().toLowerCase().equals(country.toLowerCase()) && json.get("name").toString().toLowerCase().equals(city.toLowerCase())){
-				id = (long)json.get("id");
-				break;
-			}
-		
-			
+	
+	public long findCityId(String country, String city) {
+		String key = country.toLowerCase()+city.toLowerCase();
+		if(map.containsKey(key)) {
+			return map.get(key);
 		}
-		return id;
+		return -1;
 			
 
 
